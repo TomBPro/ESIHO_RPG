6884c54d6ec7affe3a12418a17338aea05d9db6c
TomRobette <robettetom@gmail.com>
2020-03-31 20:00:45 +0200

    ESIHO-0.0.9.1

--- Esiho/src/sample/launcher.java
+++ Esiho/src/sample/launcher.java
@@ -1,8 +1,4 @@
-package ProjetPPE_RPG.projet.Esiho.src.sample;
+package sample;
 
-public class launcher {//Ceci est le laucher du jeu
-    public static void main(String[] args) {
-        MainActivity Game = new MainActivity();
-        Game.main(args);
-    }
+public class launcher {
 }
