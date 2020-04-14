472408eb049f158afaf75702caabf328d6ffa016
Thomas <satanas06660@gmail.com>
2020-04-14 17:45:52 +0200

    ESIHO-0.0.9.7

---
+++ Esiho/src/Test/ControleurThomas.java
@@ -0,0 +1,53 @@
+package Test;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.Pane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControleurThomas implements Initializable {
+
+    //Menu Écran Titre
+    @FXML
+    private Pane rootPane;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+    @FXML
+    private void startCtnButtonOnClick() throws Exception {// bouton : lance le jeu
+
+    }
+
+    @FXML
+    private void ecranCombat() throws Exception {// bouton Combat
+        //super.ecranCredits();
+        Pane pane = FXMLLoader.load(getClass().getResource("testCombatThomas.fxml"));
+        rootPane.getChildren().setAll(pane);
+    }
+
+    @FXML
+    private void startNewButtonOnClick() throws Exception {// bouton new game
+        Pane pane = FXMLLoader.load(getClass().getResource("ecranNewGame.fxml"));
+        rootPane.getChildren().setAll(pane);
+    }
+
+    @FXML
+    private void startOptnButtonOnClick() throws Exception {// bouton options
+        Pane pane = FXMLLoader.load(getClass().getResource("ecranOptions.fxml"));
+        rootPane.getChildren().setAll(pane);
+    }
+
+    //Credits
+    @FXML
+    private void optnRtrnButtonOnClick() throws Exception {//bouton retour
+        //super.ecranTitre();
+        Pane pane = FXMLLoader.load(getClass().getResource("ecranTitre.fxml"));
+        rootPane.getChildren().setAll(pane);
+    }
+}
--- Esiho/src/Test/TestThomas.java
+++ Esiho/src/Test/TestThomas.java
@@ -1,35 +1,46 @@
 package Test;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-//public class TestThomas extends Application {
-//    private Scene sceneCombat;
-//    private Stage stageCombat;
-//
-//    @Override
-//    public void start(Stage stage) throws Exception {
-//        this.stageCombat = stage;
-//        this.sceneCombat= new Scene(FXMLLoader.load(getClass().getResource("testCombatThomas.fxml")));
-//        stageCombat.setTitle("Test de combat");
-//        ecranCombat();
-//    }
-//
-//    public static void main(String[] args) {
-//        launch(args);
-//        initPerso();
-//    }
-//
-//    private static void initPerso() {
-//
-//
-//    }
-//
-//    protected void ecranCombat() throws Exception {//affiche l'écran de combat
-//        stageCombat = new Stage();
-//        stageCombat.setScene(sceneCombat);
-//        stageCombat.show();
-//    }
-//}
+public class TestThomas extends Application {
+    private Scene sceneTitre,sceneCombat;
+    private Stage primStage;
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        this.primStage = stage;
+        this.sceneCombat= new Scene(FXMLLoader.load(getClass().getResource("testCombatThomas.fxml")));
+        this.sceneTitre= new Scene(FXMLLoader.load(getClass().getResource("testMenuThomas.fxml")));
+        primStage.setTitle("Menu");
+        ecranTitre();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+        initPerso();
+    }
+
+    private static void initPerso() {
+
+
+    }
+
+    protected void ecranTitre() throws Exception {//affiche l'écran de combat
+        primStage = new Stage();
+        primStage.setScene(sceneTitre);
+        primStage.show();
+    }
+
+    public void ecranCombat() throws Exception {//affiche l'écran de combat
+        primStage = new Stage();
+        primStage.setScene(sceneCombat);
+        primStage.show();
+    }
+
+}
