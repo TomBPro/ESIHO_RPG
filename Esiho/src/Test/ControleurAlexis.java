109b6a55a90f72a8d9efe661b46ea3ef3357b291
Alexis <alexis.ogrady@outlook.fr>
2020-05-14 11:06:03 +0200

    ESIHO-0.0.11.11

--- Esiho/src/Test/ControleurAlexis.java
+++ Esiho/src/Test/ControleurAlexis.java
@@ -3,6 +3,7 @@ package Test;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
 import java.net.URL;
@@ -10,26 +11,39 @@ import java.util.ResourceBundle;
 
 public class ControleurAlexis implements Initializable {
 
-
+    @FXML
+    private GridPane gridArme, gridArmure, gridConso, gridDivers;
 
     @FXML
-    protected void afficherArme(javafx.event.ActionEvent actionEvent){
-        System.out.println("Liste arme");
+    protected void afficherArme(javafx.event.ActionEvent actionEvent) {
+        gridArme.setVisible(true);
+        gridArmure.setVisible(false);
+        gridConso.setVisible(false);
+        gridDivers.setVisible(false);
     }
 
     @FXML
     protected void afficherArmure(javafx.event.ActionEvent actionEvent){
-
+        gridArme.setVisible(false);
+        gridArmure.setVisible(true);
+        gridConso.setVisible(false);
+        gridDivers.setVisible(false);
     }
 
     @FXML
     protected void afficherConsommable(javafx.event.ActionEvent actionEvent){
-
+        gridArme.setVisible(false);
+        gridArmure.setVisible(false);
+        gridConso.setVisible(true);
+        gridDivers.setVisible(false);
     }
 
     @FXML
     protected void afficherDivers(javafx.event.ActionEvent actionEvent){
-
+        gridArme.setVisible(false);
+        gridArmure.setVisible(false);
+        gridConso.setVisible(false);
+        gridDivers.setVisible(true);
     }
 
     @Override
