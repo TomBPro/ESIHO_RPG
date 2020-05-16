6bbb1dca411d814b1198e9dedc1a4d81cdbfc133
TomRobette <robettetom@gmail.com>
2020-05-16 15:08:39 +0200

    ESIHO-0.0.11.13

--- Esiho/src/Test/TestTomR.java
+++ Esiho/src/Test/TestTomR.java
@@ -5,20 +5,14 @@ import GameData.Ressources.Contenu.Layer;
 import GameData.Ressources.Contenu.Map;
 import GameData.Ressources.Contenu.Tile;
 import javafx.application.Application;
-import javafx.embed.swing.SwingFXUtils;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-//import javafx.scene.layout.HBox;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-
-import java.awt.image.BufferedImage;
+import javafx.event.EventHandler;
+import javafx.scene.input.KeyEvent;
 import java.util.ArrayList;
 
 public class TestTomR extends Application {
@@ -28,11 +22,20 @@ public class TestTomR extends Application {
     private Integer stageWidth;
     private Integer stageHeight;
     private Canvas mapPane;
+    private Integer xJoueur;
+    private Integer yJoueur;
+    private Boolean goNorth = false;
+    private Boolean goSouth = false;
+    private Boolean goWest = false;
+    private Boolean goEast = false;
+    private Boolean running = false;
 
     @Override
     public void start(Stage stage) throws Exception {
         stageWidth=800;
         stageHeight=400;
+        xJoueur = -16+stageWidth/2;
+        yJoueur = -16+stageHeight/2;
         InitContenu contenu = new InitContenu();
         this.primStage = stage;
         primStage.setTitle("Map");
@@ -41,6 +44,47 @@ public class TestTomR extends Application {
         showLayers();
         root.getChildren().addAll(mapPane);
         this.sceneMap= new Scene(root, stageWidth, stageHeight, Color.BLACK);
+
+        sceneMap.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                switch (event.getCode()) {
+                    case UP:    goNorth = true; break;
+                    case DOWN:  goSouth = true; break;
+                    case LEFT:  goWest  = true; break;
+                    case RIGHT: goEast  = true; break;
+                    case SHIFT: running = true; break;
+                }
+            }
+        });
+
+        sceneMap.setOnKeyReleased(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                switch (event.getCode()) {
+                    case UP:    goNorth = false; break;
+                    case DOWN:  goSouth = false; break;
+                    case LEFT:  goWest  = false; break;
+                    case RIGHT: goEast  = false; break;
+                    case SHIFT: running = false; break;
+                }
+            }
+        });
+
+        if (goNorth){
+            yJoueur++;
+        }
+        if (goSouth){
+            yJoueur--;
+        }
+        if (goWest){
+            xJoueur++;
+        }
+        if (goEast){
+            xJoueur--;
+        }
+
+
         primStage.setScene(sceneMap);
 
         ecranMap();
@@ -51,7 +95,7 @@ public class TestTomR extends Application {
     }
 
     private void showLayers(){
-        Map map = new Map("M0", "test", 16,8);
+        Map map = new Map("M0", "test", 32,32);
         GraphicsContext gc = mapPane.getGraphicsContext2D();
         showOneLayer(map.getCoucheBase());
         showOneLayer(map.getCouche2());
@@ -63,7 +107,9 @@ public class TestTomR extends Application {
         for (ArrayList<Tile> ligne:couche.gridTiles) {
             Integer b = 0;
             for (Tile element:ligne) {
-                mapPane.getGraphicsContext2D().drawImage(couche.getTile(a, b).getImage(), a*16, b*16);
+                if (a*16<=-16+xJoueur*2 && b*16<=-16+yJoueur*2){
+                    mapPane.getGraphicsContext2D().drawImage(couche.getTile(a, b).getImage(), a*16, b*16);
+                }
                 b++;
             }
             a++;
