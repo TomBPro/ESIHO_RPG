dd9b22c2e4688b9039d35838c08ecab91e4d1334
TomRobette <robettetom@gmail.com>
2020-06-05 11:41:34 +0200

    0.0.14.10

--- Esiho/src/Test/TestTomRCombat.java
+++ Esiho/src/Test/TestTomRCombat.java
@@ -5,12 +5,16 @@ import GameData.Combat.Entities.Team;
 import GameData.Ressources.Contenu.InitContenu;
 import GameData.Ressources.Contenu.Pnj;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
@@ -47,52 +51,108 @@ public class TestTomRCombat extends Application {
         this.primStage = stage;
         primStage.setTitle("Combat");
         GridPane root = new GridPane();
-        GridPane rootSprites = new GridPane();
         Integer compteur = 0;
-        double nbmax1 = team1.getListePNJ().get(0).getEntite().getImage().getHeight() * 2 *team1.getListePNJ().size();
-        team1Cv = new Canvas(Math.round(stageWidth/2), Math.round(nbmax1));
-        Integer sizeT1 = Math.toIntExact(Math.round(team1.getListePNJ().size() * team1.getListePNJ().get(0).getEntite().getImage().getHeight() * 1.1 *2));
+//        double nbmax1 = team1.getListePNJ().get(0).getEntite().getImage().getHeight() * 2 *team1.getListePNJ().size();
+//        team1Cv = new Canvas(Math.round(stageWidth/2), Math.round(nbmax1));
+        GridPane team1Gd = new GridPane();
+//        Integer sizeT1 = Math.toIntExact(Math.round(team1.getListePNJ().size() * team1.getListePNJ().get(0).getEntite().getImage().getHeight() * 1.1 *2));
         for (Pnj pnj: team1.getListePNJ()) {
+            GridPane gdPn = new GridPane();
+
+            Label lbl = new Label();
+            lbl.setText(""+pnj.getNom()+"   lvl : "+pnj.getEntite().getLvl());
+            gdPn.add(lbl, 0, 0);
+
+            ProgressBar bar = new ProgressBar();
+            bar.setProgress(pnj.getEntite().getPV().getPv());
+            gdPn.add(bar, 0, 1);
+
             Image img = pnj.getEntite().getImage();
-            team1Cv.getGraphicsContext2D().drawImage(new ImageView(img).getImage(), img.getWidth()/2+stageWidth/4, compteur*sizeT1, -img.getWidth()*2, img.getHeight()*2);
+            Canvas cv = new Canvas(img.getWidth()*2, img.getHeight()*2);
+            cv.getGraphicsContext2D().drawImage(new ImageView(img).getImage(), 0, 0, -img.getWidth()*2, img.getHeight()*2);
+
+            gdPn.add(cv, 0, 2);
+            gdPn.add(new Label("TEST"), 0, 3);
+            team1Gd.add(gdPn, 0, compteur);
             compteur++;
         }
-        rootSprites.add(team1Cv, 0, 0);
+
+        root.add(team1Gd, 0, 0);
 
         compteur = 0;
-        double nbmax2 = team2.getListePNJ().get(0).getEntite().getImage().getHeight() * 2 *team2.getListePNJ().size();
-        team2Cv = new Canvas(Math.round(stageWidth/2), Math.round(nbmax2));
-        Integer sizeT2 = Math.toIntExact(Math.round(team2.getListePNJ().size() * team2.getListePNJ().get(0).getEntite().getImage().getHeight() * 1.1 *2));
-        sizeT2=Math.round(sizeT2/team2.getListePNJ().size());
+//        double nbmax2 = team2.getListePNJ().get(0).getEntite().getImage().getHeight() * 2 *team2.getListePNJ().size();
+//        team2Cv = new Canvas(Math.round(stageWidth/2), Math.round(nbmax2));
+//        Integer sizeT2 = Math.toIntExact(Math.round(team2.getListePNJ().size() * team2.getListePNJ().get(0).getEntite().getImage().getHeight() * 1.1 *2));
+//        sizeT2=Math.round(sizeT2/team2.getListePNJ().size());
+
+        GridPane team2Gd = new GridPane();
         for (Pnj pnj: team2.getListePNJ()) {
+            GridPane gdPn = new GridPane();
+
+            Label lbl = new Label();
+            lbl.setText(""+pnj.getNom()+"   lvl : "+pnj.getEntite().getLvl());
+            gdPn.add(lbl, 0, 0);
+
+            ProgressBar bar = new ProgressBar();
+            bar.setProgress(pnj.getEntite().getPV().getPv());
+            gdPn.add(bar, 0, 1);
+
             Image img = pnj.getEntite().getImage();
-            team2Cv.getGraphicsContext2D().drawImage(new ImageView(img).getImage(), img.getWidth()/2+stageWidth/4, compteur*sizeT2, img.getWidth()*2, img.getHeight()*2);
+            Canvas cv = new Canvas(img.getWidth()*2, img.getHeight()*2);
+            cv.getGraphicsContext2D().drawImage(new ImageView(img).getImage(), 0, 0, -img.getWidth()*2, img.getHeight()*2);
+
+            gdPn.add(cv, 0, 2);
+            gdPn.add(new Label("TEST"), 0, 3);
+            team2Gd.add(gdPn, 0, compteur);
             compteur++;
         }
-        rootSprites.add(team2Cv, 1, 0);
-        root.add(rootSprites, 0, 0);
-
-        GridPane rootBtn = new GridPane();
-        root.add(cbtPane, 0, 0);
+        root.add(team2Gd, 1, 0);
         Button btn1 = new Button();
         btn1.setPrefSize(Math.round(stageWidth/2), Math.round(stageHeight/6));
         btn1.setText("Bouton 1");
-        rootBtn.add(btn1, 0, 0);
+        root.add(btn1, 0, 1);
         Button btn2 = new Button();
         btn2.setPrefSize(Math.round(stageWidth/2), Math.round(stageHeight/6));
         btn2.setText("Bouton 2");
-        rootBtn.add(btn2, 0, 1);
+        root.add(btn2, 1, 1);
         Button btn3 = new Button();
         btn3.setPrefSize(Math.round(stageWidth/2), Math.round(stageHeight/6));
         btn3.setText("Bouton 3");
-        rootBtn.add(btn3, 1, 0);
+        root.add(btn3, 0, 2);
         Button btn4 = new Button();
         btn4.setPrefSize(Math.round(stageWidth/2), Math.round(stageHeight/6));
         btn4.setText("Bouton 4");
-        rootBtn.add(btn4, 1, 1);
-        root.add(rootBtn, 0, 1);
+        root.add(btn4, 1, 2);
+
+        root.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                switch (keyEvent.getCode()){
+                    case Q:
+                        root.getChildren().get(0).setVisible(false);
+                        break;
+                    case E:
+                        root.getChildren().get(1).setVisible(false);
+                }
+            }
+        });
+
+        root.setOnKeyReleased(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                switch (keyEvent.getCode()){
+                    case Q:
+                        root.getChildren().get(0).setVisible(true);
+                        break;
+                    case E:
+                        root.getChildren().get(1).setVisible(true);
+                }
+            }
+        });
 
         this.cbtScene= new Scene(root, stageWidth, stageHeight, Color.WHITE);
+
+
 
         primStage.setScene(cbtScene);
         primStage.show();
